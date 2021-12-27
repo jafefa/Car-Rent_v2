5c902bc8e366b9b0c1a338d230bed6561c2bc175
jafefa <96187683+jafefa@users.noreply.github.com>
2021-12-27 20:28:06 -0300

    envio final

---
+++ src/main/java/View/Menu_int.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author javier.fernandez55
+ */
+public interface Menu_int {
+    
+    public void initComponents();
+    
+}
